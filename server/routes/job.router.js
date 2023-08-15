bdf3ca4bc03faa8c19d21f5afae531f90e388592
sanrino <yevheniia.yelsukova@faceit.com.ua>
2023-08-15 22:42:41 +0300

    updated: job-router

--- server/routes/job.router.js
+++ server/routes/job.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const jobController = require('../controllers/job.controller')
 
-router.get("/", jobController.getAllJob);
+router.post("/", jobController.getAllJob);
 
 module.exports = router
 
