517daa6912739cb2c0d61af32331f5b280774f05
sanrino <yevheniia.yelsukova@faceit.com.ua>
2023-08-18 23:36:15 +0300

    updated: api url

--- client/src/services/jobs-service.js
+++ client/src/services/jobs-service.js
@@ -1,14 +1,14 @@
-const API_URL_JOBS = import.meta.env.VITE_API_URL;
+const API_URL_JOBS = `${import.meta.env.VITE_API_URL}/jobs`;
 
 export const JobService = {
   async getById(id) {
-    const res = await fetch(`${API_URL_JOBS}/jobs/${id}`)
+    const res = await fetch(`${API_URL_JOBS}/${id}`)
     if (!res.ok) throw new Error('Failed to fetch todos!')
     return res.json();
   },
 
   async getAll(filters) {
-    const res = await fetch(`${API_URL_JOBS}/jobs`, {
+    const res = await fetch(`${API_URL_JOBS}`, {
       method: "POST",
       body: JSON.stringify(filters),
       headers: {
@@ -19,7 +19,7 @@ export const JobService = {
   },
 
   async create(dataJob) {
-    const res = await fetch(`${API_URL_JOBS}/jobs/create`, {
+    const res = await fetch(`${API_URL_JOBS}/create`, {
       method: "POST",
       body: JSON.stringify(dataJob),
       headers: {
@@ -30,7 +30,7 @@ export const JobService = {
   },
 
   async update(dataJob) {
-    const res = await fetch(`${API_URL_JOBS}/jobs/update/${dataJob?.id}`, {
+    const res = await fetch(`${API_URL_JOBS}/update/${dataJob?.id}`, {
       method: "PUT",
       body: JSON.stringify(dataJob),
       headers: {
