bc0d99fb1b99068c04da5d67fa8bd81a470f2c45
sanrino <yevheniia.yelsukova@faceit.com.ua>
2023-11-27 20:12:24 +0200

    updated: add limit file

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(fileUpload({}));
 
 app.use("/api", routes);
